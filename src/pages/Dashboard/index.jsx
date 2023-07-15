7c4dd9fa63d93e7587a66570a6bc1d17683a72f2
itcvmaster <itvision0master@gmail.com>
2023-07-15 15:56:07 -0400

    wip: dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,247 +4,83 @@ import {
   Container,
   Row,
   Col,
-  Button,
   Card,
   CardBody,
-  Input,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Table,
+  CardTitle,
 } from 'reactstrap';
 
-const modalimage2 = "https://mintouge-s3-public.s3.eu-west-2.amazonaws.com/brand_dashboard/product/img-4.webp";
-const modalimage1 = "https://mintouge-s3-public.s3.eu-west-2.amazonaws.com/brand_dashboard/product/img-7.webp";
-
-// Pages Components
-import MonthlyEarning from './MonthlyEarning';
-import TopCities from './TopCities';
-
-//Import Breadcrumb
-import Breadcrumbs from '@components/Breadcrumb';
-
-//i18n
-import { useTranslation } from 'react-i18next';
-
+import LineColumnArea from "./LineColumnArea"
 
 const Dashboard = props => {
-  const [modal, setmodal] = useState(false);
-  const [subscribemodal, setSubscribemodal] = useState(false);
-  const { t } = useTranslation();
-  
   const { chartsData } = {};
 
   const reports = [
     { title: "Orders", iconClass: "bx-copy-alt", description: "1,235" },
     { title: "Revenue", iconClass: "bx-archive-in", description: "$35, 723" },
-    {
-      title: "Average Price",
-      iconClass: "bx-purchase-tag-alt",
-      description: "$16.2",
-    },
-    { title: "Rewards", iconClass: "bx-award", description: "100"},
-    { title: "Cash out", iconClass: "bx-archive-out", description: "$20, 500"},
-    { title: "Analytics", iconClass: "bx-analyse", description: "Consumer"}
+    { title: "Analytics", iconClass: "bx-analyse", description: "Consumer" }
   ];
 
-  useEffect(() => {
-    setTimeout(() => {
-      setSubscribemodal(true);
-    }, 2000);
-  }, []);
-
-  const [periodData, setPeriodData] = useState([]);
-  const [periodType, setPeriodType] = useState("yearly");
-
-  useEffect(() => {
-    setPeriodData(chartsData);
-  }, [chartsData]);
-
-  const onChangeChartPeriod = pType => {
-    setPeriodType(pType);
-    // dispatch(onGetChartsData(pType));
-  };
-
-  useEffect(() => {
-    // dispatch(onGetChartsData("yearly"));
-  }, []);
-
   //meta title
-  document.title="Dashboard | Mintouge - Brands Dashboard";
+  document.title = "Dashboard | Mintouge - Brands Dashboard";
 
   return (
-    <React.Fragment>
-      <div className="page-content">
-        <Container fluid>
-          {/* Render Breadcrumb */}
-          <Breadcrumbs
-            title={t("Dashboards")}
-            breadcrumbItem={t("Dashboard")}
-          />
-
-          <Row>
-            <Col xl="4">
-              <MonthlyEarning />
-            </Col>
-            <Col xl="8">
-              <Row>
-                {/* Reports Render */}
-                {reports.map((report, key) => (
-                  <Col md="4" key={"_col_" + key}>
-                    <Card className="mini-stats-wid">
-                      <CardBody>
-                        <div className="d-flex">
-                          <div className="flex-grow-1">
-                            <p className="text-muted fw-medium">
-                              {report.title}
-                            </p>
-                            <h4 className="mb-0">{report.description}</h4>
-                          </div>
-                          <div className="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
-                            <span className="avatar-title rounded-circle bg-primary">
-                              <i
-                                className={
-                                  "bx " + report.iconClass + " font-size-24"
-                                }
-                              ></i>
-                            </span>
-                          </div>
-                        </div>
-                      </CardBody>
-                    </Card>
-                  </Col>
-                ))}
-              </Row>
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xl="4">
-              <TopCities />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col lg="12">
-              {/* <LatestTranaction /> */}
-            </Col>
-          </Row>
-        </Container>
-      </div>
-
-
-      <Modal
-        isOpen={modal}
-        role="dialog"
-        autoFocus={true}
-        centered={true}
-        className="exampleModal"
-        tabIndex="-1"
-        toggle={() => {
-          setmodal(!modal);
-        }}
-      >
-        <div>
-          <ModalHeader
-            toggle={() => {
-              setmodal(!modal);
-            }}
-          >
-            Order Details
-          </ModalHeader>
-          <ModalBody>
-            <p className="mb-2">
-              Product id: <span className="text-primary">#SK2540</span>
-            </p>
-            <p className="mb-4">
-              Billing Name: <span className="text-primary">Neal Matthews</span>
-            </p>
-
-            <div className="table-responsive">
-              <Table className="table table-centered table-nowrap">
-                <thead>
-                  <tr>
-                    <th scope="col">Product</th>
-                    <th scope="col">Product Name</th>
-                    <th scope="col">Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">
-                      <div>
-                        <img src={modalimage1} alt="" className="avatar-sm" />
-                      </div>
-                    </th>
-                    <td>
-                      <div>
-                        <h5 className="text-truncate font-size-14">
-                          Wireless Headphone (Black)
-                        </h5>
-                        <p className="text-muted mb-0">$ 225 x 1</p>
-                      </div>
-                    </td>
-                    <td>$ 255</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">
-                      <div>
-                        <img src={modalimage2} alt="" className="avatar-sm" />
-                      </div>
-                    </th>
-                    <td>
-                      <div>
-                        <h5 className="text-truncate font-size-14">
-                          Hoodie (Blue)
-                        </h5>
-                        <p className="text-muted mb-0">$ 145 x 1</p>
-                      </div>
-                    </td>
-                    <td>$ 145</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Sub Total:</h6>
-                    </td>
-                    <td>$ 400</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Shipping:</h6>
-                    </td>
-                    <td>Free</td>
-                  </tr>
-                  <tr>
-                    <td colSpan="2">
-                      <h6 className="m-0 text-end">Total:</h6>
-                    </td>
-                    <td>$ 400</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              type="button"
-              color="secondary"
-              onClick={() => {
-                setmodal(!modal);
-              }}
-            >
-              Close
-            </Button>
-          </ModalFooter>
+    <div className="page-content">
+      <Container fluid>
+        <div className="page-title-container mb-4">
+          <div className="me-2">
+            <h3 className="">Dashboard</h3>
+          </div>
         </div>
-      </Modal>
-    </React.Fragment>
+
+        <Row>
+          <Col md="9">
+            <Card>
+              <CardBody>
+                <CardTitle className="mb-4">
+                  Line, Column & Area Chart{" "}
+                </CardTitle>
+                <LineColumnArea dataColors='["--bs-danger","--bs-primary", "--bs-success"]' />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col md="3">
+            {reports.map((report, key) => (
+              <Col key={"_col_" + key}>
+                <Card className="mini-stats-wid">
+                  <CardBody>
+                    <div className="d-flex">
+                      <div className="flex-grow-1">
+                        <p className="text-muted fw-medium">
+                          {report.title}
+                        </p>
+                        <h4 className="mb-0">{report.description}</h4>
+                      </div>
+                      <div className="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
+                        <span className="avatar-title rounded-circle bg-primary">
+                          <i
+                            className={
+                              "bx " + report.iconClass + " font-size-24"
+                            }
+                          ></i>
+                        </span>
+                      </div>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            ))}
+          </Col>
+        </Row>
+
+        <Row>
+
+        </Row>
+      </Container>
+    </div>
   );
 };
 
 Dashboard.propTypes = {
-  t: PropTypes.any,
   chartsData: PropTypes.any,
   onGetChartsData: PropTypes.func,
 };
