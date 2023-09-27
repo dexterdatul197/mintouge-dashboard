119702c1fd3a2fe2cd1a5fac756b3f03d2985ca9
guo9797 <guo@vaultik.com>
2023-09-27 01:30:13 +0100

    feat: add preview

---
+++ src/components/Preview.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const Preview = (props) => {
+
+    const { file } = props;
+    const [ imgData, setImgData ] = useState([]);
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+       if(file!==undefined && file!==null && file.size>0)
+       {
+          setImgData(URL.createObjectURL(file));
+          setVisible(true);
+       }
+    }, [file]);
+
+    return (
+        <>
+            { visible && <div 
+                style={{ position: 'relative', width: '200px', height: '200px'}}
+            >
+                <img src={imgData} width={200} height={200}/>
+                <div 
+                    onClick={() => setVisible(false)}
+                    style={{ position: 'absolute', right: '5px', top: '0px', fontSize: '24px'}}
+                >
+                    <i className="mdi mdi-close-circle" aria-hidden="true" />
+                </div>
+            </div> }
+        </>
+    );
+};
+
+export default Preview;
